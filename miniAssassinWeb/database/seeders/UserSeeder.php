c6d4d1e0adf1558fe6d2767fa3389ba025cea057
xtopint00 <tadeas.topinka@gmail.com>
2026-07-30 00:46:58 +0200

    [FIX] changed seeding to incorporate only seeding the admin account.

--- miniAssassinWeb/database/seeders/UserSeeder.php
+++ miniAssassinWeb/database/seeders/UserSeeder.php
@@ -12,7 +12,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(10)->create();
         User::factory()->create([
             'name' => 'admin',
             'email' => 'adminmail@example.com',
