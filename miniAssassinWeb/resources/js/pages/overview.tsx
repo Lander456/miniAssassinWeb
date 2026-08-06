434511891fc1454f632bf003a1dea9b777cdf1ce
xtopint00 <tadeas.topinka@gmail.com>
2026-08-06 11:17:00 +0200

    [FEATURE] Added a cipher for players to get the meeting point.

--- miniAssassinWeb/resources/js/pages/overview.tsx
+++ miniAssassinWeb/resources/js/pages/overview.tsx
@@ -1,5 +1,6 @@
 import { Head, Link } from '@inertiajs/react';
 import { AppHeader } from '@/components/app-header';
+import cifra from '../../images/gde.png'
 
 export default function Standby() {
 
@@ -7,19 +8,31 @@ export default function Standby() {
         <>
             <AppHeader />
 
-            <div className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
+            <div
+                className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white"
+            >
                 <Head title="Registrace je spuštěna" />
 
                 <h1 className="mb-8 text-5xl font-bold">
                     Registrace je spuštěna
                 </h1>
-                <p className="mb-8 max-w-2xl text-center text-lg text-gray-300">
+                <p className="mb-8 max-w-2xl text-center text-lg">
                     Brněnské podsvětí nikdy nespí, to ví i jeho nejmenší aktéři.
                     Proto nová společnost Aeronautica Fenomenale také nespí a
                     pod vedením exDonů Venerdi a Brutale zamířila vstříc
                     spojenectví s bájnou Geologicou Sicilianou. První prácička
                     zní jednoduše, získat informace o schodech, taková běžná
                     rutina. Člověk by si skoro ani nebral kartáček...
+                </p>
+                <p className="mb-8 max-w-2xl text-center text-lg">
+                    Orientace a proškolení nových zaměstnanců proběhne 7.8. v 16:30 na <a
+                        href={cifra}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-blue-600 underline cursor-pointer"
+                    >
+                        tomto místě.
+                    </a>
                 </p>
                 <div style={{ display: 'flex', gap: '10px' }}>
                     <Link
